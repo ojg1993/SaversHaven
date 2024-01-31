a01cb7978d1c6b5d12f603b25039539e2a2ad8a6
Jingyu <ojg1993@gmail.com>
2024-02-01 03:36:17 +0900

    feat: implement user register & token based authentication functionality
    - Implemented user registration.
    - Implemented token based login.
    - Added test cases accordingly.
    - Applied drf spectacular to improve the API documentation.

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from account import serializers
+
+
+class RegisterView(generics.CreateAPIView):
+    '''Register a new user'''
+    serializer_class = serializers.UserSerializer
+
+
+class TokenLoginView(ObtainAuthToken):
+    '''Create Token upon login'''
+    serializers_class = serializers.TokenLoginSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'nickname', 'password')}),
+        (_('Personal Info'), {"fields": ('first_name',
+                                         'middle_name',
+                                         'last_name',
+                                         'phone_number')}),
+        (_('Permissions'), {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            )}
+         ),
+        (_('Important dates'), {'fields': ('last_login', 'created_at', 'modified_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ("wide",),
+            'fields': (
+                'email',
+                'nickname',
+                'password1',
+                'password2',
+                'is_active',
+                'is_staff',
+                )}),
+    )
+    ordering = ['id']
+    list_display = ['email', 'nickname', 'is_staff']
+    readonly_fields = ['last_login', 'created_at', 'modified_at', 'nickname']
+
+
+admin.site.register(models.User, UserAdmin)
