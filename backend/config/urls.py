75d6f53781354d533a3bf8c7e5272f18fff079a0
Jingyu <ojg1993@gmail.com>
2024-02-07 22:30:21 +0900

    style: fix code lint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+        path('__debug__/', include(debug_toolbar.urls))
+                   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
