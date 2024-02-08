c936977d3d51ee1b363f137745f2a06fa582d415
Jingyu <ojg1993@gmail.com>
2024-02-08 16:51:55 +0900

    feat: implement jwt authentication
    - Configured rest_framework_simplejwt
    - Created api endpoints for issuing access token & refreshing access token

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -12,6 +13,13 @@ DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default_key')
 
+SITE_ID = 1
+
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+#     'allauth.account.auth_backends.AuthenticationBackend',
+# ]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -21,23 +29,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # Internal_apps
     'core',
-    'account',
+    'user',
     'address',
     'product',
 
     # External_apps
     'rest_framework',
-    'rest_framework.authtoken',
     'django_filters',
     'drf_spectacular',
     'corsheaders',
+
+    # Authentication
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+
+    # 'dj_rest_auth',
+    # 'dj_rest_auth.registration',
+    #
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+    # 'allauth.socialaccount.providers.google'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Cross Origin
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -45,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # "allauth.account.middleware.AccountMiddleware",  # django-allauth
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -113,7 +135,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
 }
 
 SPECTACULAR_SETTINGS = {
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from user import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'user'
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.TokenLoginView.as_view(), name='login'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
