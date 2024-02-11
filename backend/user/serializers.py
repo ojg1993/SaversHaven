37e296cfd4908f57e939a3ab8023ac66383c0bf5
Jingyu <ojg1993@gmail.com>
2024-02-11 22:39:44 +0900

    chore: remove token(test) auth related serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -15,32 +15,6 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name', 'last_name', 'phone_number'
         )
         extra_kwargs = {'password': {'write_only': True, 'min_length': 10}}
-    #
-    # def create(self, validated_data):
-    #     return get_user_model().objects.create_user(**validated_data)
-
-
-# class TokenLoginSerializer(serializers.Serializer):
-#     email = serializers.EmailField()
-#     password = serializers.CharField(
-#         style={'input_type': 'password'},
-#         trim_whitespace=False
-#     )
-#
-#     def validate(self, attrs):
-#         email = attrs.get('email')
-#         password = attrs.get('password')
-#         user = authenticate(
-#             request=self.context.get('request'),
-#             username=email,
-#             password=password
-#         )
-#
-#         if not user:
-#             msg = _('Unable to log in with provided credentials')
-#             raise serializers.ValidationError(msg, code='authentication')
-#         attrs['user'] = user
-#         return attrs
 
 class CustomRegisterSerializer(RegisterSerializer):
     username = None
