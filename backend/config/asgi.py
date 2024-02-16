be5d51b5c5d8b5b0cbea91ed1444b70600d2ab5d
Jingyu <ojg1993@gmail.com>
2024-02-16 16:40:15 +0900

    style: apply lint tools

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,10 +1,9 @@
 import os
 
-from django.core.asgi import get_asgi_application
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
 if os.environ.get('DEBUG'):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
