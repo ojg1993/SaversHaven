7b6f0da3ff23d19a3e00a5515a44682dbe9176f5
Jingyu <ojg1993@gmail.com>
2024-03-03 23:52:49 +0900

    chore: configure production settings
    - local to prod: asgi.py, manage.py
    - RDS to local postgre: prod.py
    - static file configurations: urls.py
    - unnecessary setting removal in nginx Dockerfile
    - container setup: docker-compose.prod.yml

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -41,9 +41,10 @@ urlpatterns = [
          name='api-docs'),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
